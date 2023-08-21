053dcfa7eecd4359ffe2ec67ab003e83aa00bd6c
victoirempkn <96738243+victoiremupikano@users.noreply.github.com>
2023-08-21 15:49:55 +0200

    code pour les classes de remplissage des cbo

--- FinanceManager/Services/CBO/Db.cs
+++ FinanceManager/Services/CBO/Db.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Commercial_Management.Services.CBO
+namespace FinanceManager.Services.CBO
 {
     class Db
     {
--- FinanceManager/Services/CBO/cboDb.cs
+++ FinanceManager/Services/CBO/cboDb.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Commercial_Management.Services.CBO
+namespace FinanceManager.Services.CBO
 {
     class cboDb
     {
