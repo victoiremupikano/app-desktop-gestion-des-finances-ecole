385b88381ae35a67d979cc85d74aad96260321f0
victoirempkn <96738243+victoiremupikano@users.noreply.github.com>
2023-08-21 17:41:06 +0200

    corrections sur les controllers, Program

--- FinanceManager/Program.cs
+++ FinanceManager/Program.cs
@@ -16,7 +16,7 @@ namespace FinanceManager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmConnexion());
+            Application.Run(new frmConnecting());
         }
     }
 }
