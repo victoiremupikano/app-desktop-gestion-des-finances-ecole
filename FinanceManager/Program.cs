6b6550002df6db89199c2424f6e0f25bc88d6271
victoirempkn <96738243+victoiremupikano@users.noreply.github.com>
2023-08-21 15:00:23 +0200

    ajout frm connexion

--- FinanceManager/Program.cs
+++ FinanceManager/Program.cs
@@ -16,7 +16,7 @@ namespace FinanceManager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmConnexion());
         }
     }
 }
